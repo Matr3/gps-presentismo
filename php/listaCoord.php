3a0f9734434232af05cbe1a02a2e8bf72a3ccedf
Matr3 <tremari.marce@gmail.com>
2022-11-07 18:32:32 -0300

    test login

--- php/listaCoord.php
+++ php/listaCoord.php
@@ -3,7 +3,7 @@
 	require "databaseconnectcoord.php";
 	
 	session_start();
-    $query = $mysqli->query("SELECT * FROM coordinadores");
+    $query = $conn->query("SELECT * FROM coordinadores");
     while ($valores=mysqli_fetch_array($query)) {
     echo'<option value="'.$valores[coordinador_name].'">'.$valores[coordinador_name].'</option>';
     }
--- php/listaObj.php
+++ php/listaObj.php
@@ -3,7 +3,7 @@
 	require "databaseconnectcoord.php";
 	
 	session_start();
-    $query = $mysqli->query("SELECT * FROM objetivo");
+    $query = $conn->query("SELECT * FROM objetivo");
     while ($valores=mysqli_fetch_array($query)) {
         echo'<option value="'.$valores[servicio_name].'">'.$valores[servicio_name].'</option>';
     }
