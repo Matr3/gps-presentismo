1f527da24031e8772beab3291ee67f9ff5b94704
Matr3 <tremari.marce@gmail.com>
2022-11-10 21:42:40 -0300

    selectores js php mysql

--- js/datas.js
+++ js/datas.js
@@ -58,8 +58,25 @@ function checkTime(i) {
   return i;
 }
 
+function selector() {
+  $.post("php/listaObj.php", function(data) {
+      const selector1 =  document.querySelectorAll("#SelectObj");
+      for(var i = 0; i < selector1.length; i++){
+      $(selector1[i]).html(data);
+      }
+  });
+  $.post("php/listaCoord.php", function(data) {
+      const selector =  document.querySelectorAll("#SelectCoord");
+      for(var i = 0; i < selector.length; i++){
+      $(selector[i]).html(data);
+      }
+  });
+}
+
 window.onload = function (){
+  selector();
   fecha();
   hora();
   startTime();
+  
 }
